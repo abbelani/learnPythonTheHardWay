d34210408e38a8c298c449f2b9e2f27f3deef3ad
Ab Belani <abbelani@gmail.com>
2011-10-25 13:59:10 -0700

    First 14 excercises

---
+++ ex4.py
@@ -0,0 +1,21 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
+
+#Extra Credit
+# He called the wrong variable, its carpool_capacity not car_pool_capacity
+# 1 use 4.0 because decimals are still important in the calculation
+# 2 
